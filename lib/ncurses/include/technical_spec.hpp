4fd7786dea5d486f5e79dea3c4dbb5212233084d
Baptiste Veyssiere <baptiste.veyssiere@epitech.eu>
2017-04-03 16:24:02 +0200

    technical_spec

---
+++ lib/ncurses/include/technical_spec.hpp
@@ -0,0 +1,17 @@
+//
+// technical_spec.hpp for Project-Master in /home/veyssi_b/rendu/tek2/CPP/cpp_arcade
+//
+// Made by Baptiste Veyssiere
+// Login   <veyssi_b@epitech.net>
+//
+// Started on  Mon Apr  3 16:17:28 2017 Baptiste Veyssiere
+// Last update Mon Apr  3 16:22:13 2017 Baptiste Veyssiere
+//
+
+#ifndef __TECHNICAL_SPEC_HPP__
+# define __TECHNICAL_SPEC_HPP__
+
+# define WINSIDE	816
+# define FPS		30
+
+#endif // !__TECHNICAL_SPEC_HPP__
