1760bee7c378f2fdc7fd2815266605f095e48e70
ilyas semmaoui <ilyas.semmaoui@epitech.eu>
2017-04-08 12:47:46 +0200

    shiftopengl

--- lib/opengl/src/libopengl.cpp
+++ lib/opengl/src/libopengl.cpp
@@ -5,7 +5,7 @@
 // Login   <ilyas.semmaoui@epitech.eu>
 //
 // Started on  Tue Apr  4 00:59:31 2017 ilyas semmaoui
-// Last update Sat Apr  8 12:13:02 2017 ilyas semmaoui
+// Last update Sat Apr  8 12:47:14 2017 ilyas semmaoui
 //
 
 #include <iostream>
@@ -119,8 +119,8 @@ void	libopengl::Loop_display(const t_map &map) const {
   GLdouble	angle;
 
   glClear(GL_COLOR_BUFFER_BIT);
-  x_size = WINSIDE / map.width;
-  y_size = WINSIDE / map.height;
+  x_size = static_cast<double>(WINSIDE) / static_cast<double>(map.width);
+  y_size = static_cast<double>(WINSIDE) / static_cast<double>(map.height);
   if (textures.find("bg") == textures.end())
     throw library_error("Failed to find a texture !");
   putBackground();
@@ -130,8 +130,8 @@ void	libopengl::Loop_display(const t_map &map) const {
       x = -1;
       while (++x < map.width)
 	{
-	  posx = (x * x_size) * 2 / WINSIDE;
-	  posy = (y * y_size) * 2 / WINSIDE;
+	  posx = (static_cast<double>(x) * x_size + map.map[y][x].shiftx) * 2.0 / static_cast<double>(WINSIDE);
+	  posy = (static_cast<double>(y) * y_size + map.map[y][x].shifty) * 2.0 / static_cast<double>(WINSIDE);
 	  if ((file = this->tile_to_file(map.map[y][x])) != "")
 	    {
 	      if (textures.find(file) == textures.end())
